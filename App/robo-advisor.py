7429e6a56ac53bf2f3dc51950674997f164b5331
jts140 <77750110+jts140@users.noreply.github.com>
2021-03-04 00:37:07 -0500

    added refresh data

--- App/robo-advisor.py
+++ App/robo-advisor.py
@@ -13,7 +13,10 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+print(last_refreshed)
+#breakpoint()
 
 #info outputs
 
@@ -26,7 +29,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
